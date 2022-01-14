15f39088c42f201730d367f39f412af1ac6849dc
Spoorti Aparanji <88270835+spoorti016@users.noreply.github.com>
2022-01-14 13:10:16 +0530

    Add files via upload
    added the python files for bubble sort and quick sort

---
+++ bub_srt.py
@@ -0,0 +1,26 @@
+# Extension Bubble Sort Code
+# importing time module which handles time related tasks
+import time
+
+
+# function to implement bubble sort by passing the following parameters:
+# data is passed for the set of unsorted data values
+# drawdata is used to generate the data bars
+# timer is for the speed range
+def bubble(data, drawData, timer):
+    n = len(data)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if data[j] > data[j + 1]:
+                #swapping
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+                # if swapped then color becomes Green else stays Red
+                drawData(data, ['Green' if x == j + 1 else 'Red' for x in range(len(data))])
+
+                time.sleep(timer)  # to add delay in execution of a program
+
+    # sorted elements generated with Green color
+    drawData(data, ['Green' for x in range(len(data))])
